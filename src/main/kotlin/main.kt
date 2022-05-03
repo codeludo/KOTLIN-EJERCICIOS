e5b5b3aeb903f7eab7c3571642111b27a71238ee
camilo <camiloacevedomoreno@gmail.com>
2022-05-02 19:58:10 -0500

    clase abstracta empleado - clases de empleados

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,74 @@
 import java.util.Scanner
+// Clase abstracta empleado
+abstract class Empleado(){
+    abstract val valorHora: Int
+    abstract val horasMes: Int
+    abstract var salario: Int
+    abstract val tieneBono: Boolean
+    val valorBono: Int = 0
+
+    fun calcularBono() = 10 * valorHora
+
+    fun calcularSalario(bono: Boolean = false): Int{
+
+        salario = if(bono) {
+            val valorBono = calcularBono()
+            valorHora * horasMes + valorBono
+        } else{
+            valorHora * horasMes + valorBono
+        }
+        return salario
+    }
+}
+
+// Clase Gerente
+class Gerente: Empleado() {
+    override val valorHora: Int = 200
+    override val horasMes: Int = 200
+    override val tieneBono: Boolean = true
+
+    override var salario: Int = calcularSalario()
+}
+
+// Clase Operario
+class Operario: Empleado() {
+    override val valorHora: Int = 10
+    override val horasMes: Int = 230
+    override val tieneBono: Boolean = true
+
+    override var salario: Int = calcularSalario(tieneBono)
+}
+
+// Clase Contador
+class Contador: Empleado() {
+    override val valorHora: Int = 50
+    override val horasMes: Int = 200
+    override val tieneBono: Boolean = false
+
+    override var salario: Int = calcularSalario()
+}
+
 
 /**
  * funcion para pagar el salario al empleado por código
  */
 fun pagarSalario(codEmpleado: Int){
-    var salario: Double = 0.0
+    var salario = 0
     when (codEmpleado) {
-        1 -> salario =  calcularSalario(200, 200, false)
-        2 -> salario = calcularSalario(230, 10, true)
-        3 -> salario = calcularSalario(200, 50, false)
+        1 -> {
+            val gerente = Gerente()
+            salario = gerente.salario
+        }
+        2 -> {
+            val operario = Operario()
+            salario = operario.salario
+        }
+        3 -> {
+            val contador = Contador()
+            salario = contador.salario
+        }
     }
     println("Cod. empleado: $codEmpleado, Salario: $salario")
-}
-
-/**
- * calcula el salario por los datos
- * hora mes
- * valor hora
- * bono
- */
-fun calcularSalario(horasMes: Int, valorHora: Int, tieneBono: Boolean): Double {
-    var valorBono = 0.0
-    if(tieneBono){
-        valorBono = 10 * valorHora.toDouble()
-    }
-    return (horasMes*valorHora)+valorBono
 }
 
 fun main(){
